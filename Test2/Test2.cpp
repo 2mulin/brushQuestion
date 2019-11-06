a1bc5dde4291cb7a611341aec6cc96f838f8bdf1
MenQi <1730911462@qq.com>
2019-11-06 19:55:42 +0800

    添加项目文件。

---
+++ Test2/Test2.cpp
@@ -0,0 +1,34 @@
+﻿// Test2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+class A 
+{
+public:
+	static int a;
+private:
+	static int b;
+};
+
+int A::a = 10;
+int A::b = 20;
+
+//返回X转化为二进制后的1的个数
+int func(int x)
+{
+	int countx = 0;
+	while (x)
+	{
+		countx++;
+		x = x & (x - 1);
+	}
+	return countx;
+}
+
+int main()
+{
+	A instance;
+	std::cout << A::a << std::endl << instance.a << std::endl;
+	std::cout << func(22);
+}
