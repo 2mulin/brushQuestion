1a5bd3b122ec76942efcb1a061a5202edb0e0eb8
MenQi <1730911462@qq.com>
2020-01-20 23:04:56 +0800

    2020/1/20 23:04

---
+++ problem16464/problem16464.cpp
@@ -0,0 +1,53 @@
+﻿// problem16464.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 题目：神奇的幻方 题目链接：https://ac.nowcoder.com/acm/problem/16464
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+const int N = 40;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[N][N]{ 0 };
+    int x = 1, y = n / 2 + 1;
+    for (int i = 1; i <= n * n; i++)
+    {
+        arr[x][y] = i;
+        if (x == 1 && y != n)
+        {
+            x = n;
+            y += 1;
+        }
+        else if (x != 1 && y == n)
+        {
+            x -= 1;
+            y = 1;
+        }
+        else if (x == 1 && y == n)
+        {
+            x += 1;
+        }
+        else if (x != 1 && y != n && !arr[x - 1][y + 1])
+        {
+            x -= 1;
+            y += 1;
+        }
+        else
+        {
+            x += 1;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            cout << arr[i][j] << ' ';
+        }
+        cout << arr[i][n] << std::endl;
+    }
+    return 0;
+}
+
--- problem16589/problem16589.cpp
+++ problem16589/problem16589.cpp
@@ -1,9 +1,49 @@
 ﻿// problem16589.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+// 题目：机器翻译 链接：https://ac.nowcoder.com/acm/problem/16589
+// 运用了双端队列，可以在头插入，删除，也可以在尾部插入，删除
 
 #include <iostream>
+#include <deque>
+using std::deque;
+using std::cin;
+using std::cout;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int m, n;
+    cin >> m >> n;
+    deque<int> q;                           //存放存入内存的单词,最多存入m个
+    int ans = 0;                            //记录查询次数
+    for (int i = 0; i < n; i++)
+    {
+        int num = 0;
+        cin >> num;
+        if (q.empty())
+        {
+            q.push_back(num);
+            ans++;
+        }
+        else
+        {
+            bool isExist = false;
+            int max = q.size();
+            for (int i = 0; i < max; i++)
+            {
+                int x = q.front();
+                if (x == num)               //无需查找，内存中有
+                    isExist = true;
+                q.pop_front();
+                q.push_back(x);
+            }
+            if (!isExist)                   //如果内存中没有，加入内存
+            {
+                ans++;
+                if (q.size() >= m)          //超过最大存储
+                    q.pop_front();
+                q.push_back(num);
+            }
+        }
+    }
+    cout << ans << std::endl;
+    return 0;
 }
