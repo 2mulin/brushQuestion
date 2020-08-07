e4a5547dc0eb9901d19fb37ff387d7bee2876e95
linglong <1730911462@qq.com>
2020-08-07 21:34:53 +0800

    8月7日两题

---
+++ LeetCode/80_removeDuplicates.cpp
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * @author reddragon
+ * @date 2020/8/7
+ * @brief 删除数组中的重复项2 midden
+ * 每个数字最多重复2次
+ * 双指针法：一个指针指向当时满足条件的位置，一个指向需要继续测试的位置。
+ * 同时需要计数，因为每个数字可以重复两次。
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int> &nums)
+{
+    int len = 0;
+    int count = 0;
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        if (count < 2)
+        {
+            nums[len++] = nums[i];
+            count++;
+
+            // 可能出现1直接到0.（有些数只有一个，count不会大于2）
+            if (i + 1 < nums.size() && nums[len - 1] != nums[i + 1])
+                count = 0;
+        }
+        else
+        { // 干掉剩余重复2个以上的数字
+            while (i < nums.size() && nums[i] == nums[len - 1])
+                i++;
+            i--;
+            count = 0;
+        }
+    }
+    return len;
+}
+
+int main()
+{
+    vector<int> arr{0, 0, 1, 1, 1, 1, 2, 3, 3};
+    removeDuplicates(arr);
+    for (auto i : arr)
+        cout << i << '\t';
+    return 0;
+}
---
+++ LeetCode/81_search.cpp
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * @author reddragon
+ * @date 2020/8/7
+ * @brief 搜索旋转排序数组 midden
+ * 没有官方题解，不知道自己到底get到点了没，下面代码能过
+ *******************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 前半段使用顺序查找，后半段使用二分查找
+bool search(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    for (; low <= high; low++)
+    {
+        if (nums[low] == target)
+            return true;
+        // 逆序，说明在此处旋转了
+        if (low < high && nums[low] > nums[low + 1])
+            break;
+    }
+    low++;
+    // 对后半部分进行二分查找
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] > target)
+            high = mid - 1;
+        else if (nums[mid] < target)
+            low = mid + 1;
+        else
+            return true;
+    }
+    return false;
+}
+
+// 顺序查找 O(n)
+// bool search(vector<int> &nums, int target)
+// {
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         if (nums[i] == target)
+//             return true;
+//     }
+//     return false;
+// }
+
+int main()
+{
+    vector<int> arr{2, 5, 6, 0, 0, 1, 2};
+    cout << search(arr, 2);
+    return 0;
+}
