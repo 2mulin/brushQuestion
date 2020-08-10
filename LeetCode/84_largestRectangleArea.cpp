b941416533eea992fdde352d2a0dcf67b26b8fbe
linglong <1730911462@qq.com>
2020-08-10 17:26:38 +0800

    隔了一天就看不懂了，代码添加注释

--- LeetCode/84_largestRectangleArea.cpp
+++ LeetCode/84_largestRectangleArea.cpp
@@ -3,7 +3,7 @@
  * @date 2020/8/9
  * @brief hard 柱状图里最大的矩形面积
  * 自己想的暴力解法是真垃圾，官方连暴力解法都比我的好，多多反思
- * 费时一上午，通过研究题解，终于知道了单调栈，在这里怎么用。
+ * 费时一上午，通过研究题解，终于知道了单调栈在这里怎么用。
  * 后面发现可以不使用stack，也可以算，速度还快些，是单调栈的思路，但是不需要辅助stack
  * 
  * 题解还有一个for一次就可以得到left和right数组的优化
@@ -14,34 +14,6 @@
 #include <stack>
 using namespace std;
 
-// 暴力解法：时间O(n*n*n) 空间O(1)
-// inline int getMinHeight(vector<int> &heights, int i, int j)
-// {
-//     int res = INT_MAX;
-//     for (int k = i; k <= j; k++)
-//         if (heights[k] < res)
-//             res = heights[k];
-//     return res;
-// }
-
-// int largestRectangleArea(vector<int> &heights)
-// {
-//     if (heights.empty())
-//         return 0;
-//     int ans = 0;
-//     int size = heights.size();
-//     vector<int> arr[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         for (int j = i; j < size; j++)
-//         {
-//             int minH = getMinHeight(heights, i, j);
-//             ans = max(ans, minH * (j - i + 1));
-//         }
-//     }
-//     return ans;
-// }
-
 // 暴力解法， 时间O(N*N),空间O(1)
 // int largestRectangleArea(vector<int> &heights)
 // {
@@ -50,18 +22,19 @@ using namespace std;
 //         return 0;
 //     int res = 0;
 //     for(int i = 0; i < size; i++)
-//     {
+//     {// 包含i这一列的矩形
 //         int left = i , right = i;
 //         while(left >= 0 && heights[left] >= heights[i])
-//             left--;
+//             left--;// 从i往左找到第一个小于heights[i]的位置记为left
 //         while(right < size && heights[right] >= heights[i])
-//             right++;
+//             right++;// 从i往右找到第一个小于heights[i]的位置记为right
+//         // 得到包含这一列的矩形最大面积
 //         res = max(res, heights[i] * (right - left - 1));
 //     }
 //     return res;
 // }
 
-// 利用单调栈求出left数组和right数组
+// 利用单调栈求出left数组和right数组。时间复杂度：O(N)  空间复杂度: O(N)
 int largestRectangleArea(vector<int> &heights)
 {
     int size = heights.size();
