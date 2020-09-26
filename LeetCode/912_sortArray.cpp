b254011ab0676b407e1ea4df3212a50feaee0e5b
linglong <1730911462@qq.com>
2020-09-26 20:06:44 +0800

    排序算法写一遍

--- LeetCode/912_sortArray.cpp
+++ LeetCode/912_sortArray.cpp
@@ -2,7 +2,13 @@
  * @author reddragon
  * @date 2020/9/25
  * @brief medium 排序
+ * 
  * 1.快速排序quicksort
+ * 快排的思路就不用说了，很清楚了
+ * 主要是要注意细节。一定记得实时更新下标，就是每一次循环都在缩小
+ * 下一次循环的范围
+ * 
+ * 2.归并排序
  * 
  **********************************************/
 
@@ -11,37 +17,59 @@
 #include <algorithm>
 using namespace std;
 
+void merge()
+{
+
+}
+
+// 归并排序
+void partition(vector<int>& arr, int begin, int end)
+{
+    if(arr.size() <= 1)
+        return;
+    int meuidm = arr.size() / 2;
+    
+}
+
+void mergeSort(vector<int>& arr)
+{
+    partition(arr, 0, arr.size() - 1);
+    return;
+}
+
 // 快排
 void quicksort(vector<int> &nums, int begin, int end)
 {
     if (end - begin <= 0)
         return;
+    srand(time(nullptr));
     int key = rand() % (end - begin) + begin; // 随机选定一个基点
-    bool isEnd = false;
-    while (!isEnd)
+    int i = begin, j = end;
+    while (i < j)
     {
-        isEnd = true;
         // 从右到左
-        for (int i = end; i > key; i--)
+        while(j > key)
         {
-            if (nums[i] < nums[key])
+            if(nums[j] < nums[key])
             {
-                swap(nums[i], nums[key]);
-                key = i;
-                isEnd = false;
+                swap(nums[j], nums[key]);
+                key = j;// 记住这个不能少
                 break;
             }
+            else
+                j--;// 记住这个不能少
         }
         // 从左到右
-        for (int i = begin; i < key; i++)
+        while(i < key)
         {
             if (nums[i] > nums[key])
             {
                 swap(nums[i], nums[key]);
-                key = i;
-                isEnd = false;
+                key = i;// 记住这个不能少
                 break;
             }
+            else
+                i++;// 记住这个不能少
         }
     }
     quicksort(nums, begin, key - 1);
@@ -49,14 +77,17 @@ void quicksort(vector<int> &nums, int begin, int end)
     return;
 }
 
-// quicksort
 vector<int> sortArray(vector<int> &nums)
 {
-    quicksort(nums, 0, nums.size() - 1);
+    partition(nums, 0, nums.size() - 1);
     return nums;
 }
 
 int main()
 {
+    vector<int> arr{1,231,5,3,1256,2,1654,23,165,432};
+    sortArray(arr);
+    for(auto i : arr)
+        cout << i << '\t';
     return 0;
 }
