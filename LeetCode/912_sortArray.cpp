5d759a5961ee3b70de349a09d3f0b5cd58ccb793
linglong <1730911462@qq.com>
2020-10-14 16:12:23 +0800

    整理一下排序算法

--- LeetCode/912_sortArray.cpp
+++ LeetCode/912_sortArray.cpp
@@ -1,93 +1,173 @@
-/***********************************************
+/********************************************************************************************************
  * @author reddragon
- * @date 2020/9/25
- * @brief medium 排序
+ * @date 2020/10/14
+ * @brief medium 排序数组
  * 
  * 1.快速排序quicksort
- * 快排的思路就不用说了，很清楚了
- * 主要是要注意细节。一定记得实时更新下标，就是每一次循环都在缩小
- * 下一次循环的范围
+ * 快速排序的主要思路就是通过划分将待排序的序列分成前后两部分，其中前一部分
+ * 的数据都比后一部分的小，然后递归调用函数对两部分的序列进行快速排序，以此
+ * 让整个序列达到有序。
+ * 时间复杂度：O(nlogn)     空间复杂度: O(h)其中h为递归调用的栈空间
  * 
- * 2.归并排序
+ * 2.归并排序meageSort
+ * 归并排序利用分治的思想来对序列排序，对一个长为n的数组排序，我们先将其分成
+ * 两个n/2长度的数组，每次先递归调用函数使两个子序列有序，然后我们再
+ * 线性合并O(n)两个子序列。
+ * 时间复杂度：O(nlogn)      空间复杂度：O(n+logn)
+ * 空间复杂度：O(n)。我们需要额外 O(n) 空间的 tmp 数组，且归并排序递归调用的层数最深为logn，
+ * 所以我们还需要额外的 O(logn) 的栈空间，所需的空间复杂度即 O(n+logn)=O(n)。
  * 
- **********************************************/
-
+ * 3.堆排序heapSort
+ * 堆排序的思想就是先将待排序的序列建成大根堆，使得每个父节点的元素大于等于它的子节点。
+ * 此时整个序列最大值即为堆顶元素，我们将其与末尾元素交换，使末尾元素为最大值，
+ * 然后再调整堆顶元素使得剩下的 n-1个元素仍为大根堆，再重复执行以上操作我们即能得到一个有序的序列。
+ * 时间复杂度: O(nlogn)     空间复杂度：O(1)
+ *********************************************************************************************************/
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include <algorithm>
 using namespace std;
-
-void merge()
+/* 快速排序
+void quicksort(vector<int> &arr, int b, int e)
 {
-
-}
-
-// 归并排序
-void partition(vector<int>& arr, int begin, int end)
-{
-    if(arr.size() <= 1)
-        return;
-    int meuidm = arr.size() / 2;
-    
-}
-
-void mergeSort(vector<int>& arr)
-{
-    partition(arr, 0, arr.size() - 1);
-    return;
-}
-
-// 快排
-void quicksort(vector<int> &nums, int begin, int end)
-{
-    if (end - begin <= 0)
+    if(b >= e)
         return;
     srand(time(nullptr));
-    int key = rand() % (end - begin) + begin; // 随机选定一个基点
-    int i = begin, j = end;
-    while (i < j)
+    int pos = rand() % (e - b) + b;
+    cout << pos << endl;
+    int low = b, hi = e;
+    while(low < hi)
     {
-        // 从右到左
-        while(j > key)
+        while(low < pos)
         {
-            if(nums[j] < nums[key])
+            if(arr[low] > arr[pos])
             {
-                swap(nums[j], nums[key]);
-                key = j;// 记住这个不能少
+                swap(arr[low], arr[pos]);
+                pos = low;
                 break;
             }
             else
-                j--;// 记住这个不能少
+                low++;
         }
-        // 从左到右
-        while(i < key)
-        {
-            if (nums[i] > nums[key])
+        while(pos < hi)
+        { 
+            if(arr[hi] < arr[pos])
             {
-                swap(nums[i], nums[key]);
-                key = i;// 记住这个不能少
+                swap(arr[hi], arr[pos]);
+                pos = hi;
                 break;
             }
             else
-                i++;// 记住这个不能少
+                hi--;
         }
     }
-    quicksort(nums, begin, key - 1);
-    quicksort(nums, key + 1, end);
+    quicksort(arr, b, pos - 1);
+    quicksort(arr, pos + 1, e);
+} */
+
+/* 归并排序
+void meageSort(vector<int> & arr, int low, int hi)
+{
+    if(hi <= low)
+        return;
+    // 分
+    int mid = (low + hi) / 2;
+    meageSort(arr, low, mid);
+    meageSort(arr, mid + 1, hi);
+
+    // 归并
+    vector<int> temp(hi - low + 1);
+    int i = low, j = mid + 1, k = 0;
+    while(i <= mid && j <= hi)
+    {
+        if(arr[i] < arr[j])
+        {
+            temp[k++] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k++] = arr[j];
+            j++;
+        }
+    }
+    while(i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+    while(j <= hi)
+        temp[k++] = arr[j++];
+
+    k = 0;
+    for(int i = low; i <= hi; i++)
+        arr[i] = temp[k++];
+    return;
+} */
+
+// 堆排序
+void heapSort(vector<int> &arr)
+{
+    // 建大顶堆
+    int len = arr.size();
+    for (int i = len / 2 - 1; i >= 0; i--)
+    {
+        // 下滤
+        int pos = i;
+        while ((2 * pos + 1 < len && arr[pos] < arr[2 * pos + 1]) || (2 * pos + 2 < len && arr[pos] < arr[2 * pos + 2]))
+        {
+            int max = 2 * pos + 1;
+            // 需要考虑只有一个叶子的情况，即2*pos + 2是空的
+            if (2 * pos + 2 < len)
+            {
+                if (arr[2 * pos + 1] > arr[2 * pos + 2])
+                    max = 2 * pos + 1;
+                else
+                    max = 2 * pos + 2;
+            }
+            else
+                max = 2 * pos + 1;
+            swap(arr[pos], arr[max]);
+            pos = max;
+        }
+    }
+
+    while (len--)
+    {
+        swap(arr[len], arr[0]);
+        int pos = 0;
+        while ((2 * pos + 1 < len && arr[pos] < arr[2 * pos + 1]) || (2 * pos + 2 < len && arr[pos] < arr[2 * pos + 2]))
+        {
+            int max = 2 * pos + 1;
+            // 需要考虑只有一个叶子的情况，即2*pos + 2是空的
+            if (2 * pos + 2 < len)
+            {
+                if (arr[2 * pos + 1] > arr[2 * pos + 2])
+                    max = 2 * pos + 1;
+                else
+                    max = 2 * pos + 2;
+            }
+            else
+                max = 2 * pos + 1;
+            swap(arr[pos], arr[max]);
+            pos = max;
+        }
+    }
     return;
 }
 
-vector<int> sortArray(vector<int> &nums)
+void sortArray(vector<int> &arr)
 {
-    partition(nums, 0, nums.size() - 1);
-    return nums;
+    // quicksort(arr,0,arr.size() -1);
+    // meageSort(arr, 0, arr.size() - 1);
+    heapSort(arr);
 }
 
 int main()
 {
-    vector<int> arr{1,231,5,3,1256,2,1654,23,165,432};
+    vector<int> arr{2, 1, 4, 3, 6, 3, 9, 8};
     sortArray(arr);
-    for(auto i : arr)
+    for (int i : arr)
         cout << i << '\t';
     return 0;
 }
