1a748c0f5607930f3f841bc8e46aa30e53e7cc70
linglong <1730911462@qq.com>
2020-11-10 23:19:02 +0800

    最短路径

--- Others/最短路径.cpp
+++ Others/最短路径.cpp
@@ -1,64 +1,53 @@
 /********************************************************************************************
  * @author reddragon
- * @date 2020/11/9
+ * @date 2020/11/10
  * @brief 最短路径
  * 
- * 未完成，明天继续
- * 
  * 1. 最短路径
- * 
- * 时间复杂度：O(NlogN)        空间复杂度：O(N)
+ * 最短路径有两种，Dijkstra和Floyd，但是Dijkstra不能处理 负边 情况。
+ * 所以这里采用Floyd算法。
+ * 时间复杂度：O(N*N*N)        空间复杂度：O(N*N)
  ********************************************************************************************/
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <climits>
 #include <algorithm>
 using std::cin;
 using std::cout;
 using std::vector;
-using std::queue;
 
+const int MAX = 10005;
 
 int main()
 {
     int n,m;
     while(cin >> n >> m)
     {
-        vector<vector<int> > Matrix(n + 1, vector<int>(n + 1, INT_MAX));
+        // 邻接矩阵
+        vector<vector<int> > Matrix(n + 1, vector<int>(n + 1, MAX));
         for(int i = 0; i < m; ++i)
         {
             int x,y,val;
             cin >> x >> y >> val;
             Matrix[x][y] = val;
         }
-        vector<int> dist(n + 1);// 存放1号点到其他顶点的距离
-        vector<bool> isVisit(n + 1, false);
-        dist[1] = 0;
-        isVisit[1] = true;
-        // vector<int> path; 
-        queue<int> qu;
-        qu.push(1);
-        while(!qu.empty())
+        // 对角线全部初始化为0
+        for(int i = 1; i <= n; ++i)
+            Matrix[i][i] = 0;
+        // Floyd算法实现
+        for(int k = 1; k <= n; ++k)
         {
-            int src = qu.front();
-            isVisit[src] = true;
-            qu.pop();
-            // 检查所有row顶点的临界顶点
-            for(int dst = 1; dst <= n; ++dst)
+            for(int i = 1; i <= n; ++i)
             {
-                if(Matrix[src][dst] != INT_MAX && !isVisit[dst])
+                for(int j = 1; j <= n; ++j)
                 {
-                    qu.push(dst);
-                    // 
-                    dist[dst] = std::min(dist[src] + Matrix[src][dst], dist[dst]);
+                    Matrix[i][j] = std::min(Matrix[i][j], Matrix[i][k] + Matrix[k][j]);
                 }
             }
         }
 
         // 输出1到其他所有顶点的最短距离
         for(int i = 2; i <= n; ++i)
-            cout << dist[i] << std::endl;
+            cout << Matrix[1][i] << std::endl;
     }
     return 0;
 }
