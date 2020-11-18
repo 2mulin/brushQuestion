5273cc06f1177504d3edf853eed9d9343985cfa0
linglong <1730911462@qq.com>
2020-11-18 23:30:12 +0800

    坚持

--- Others/K_num.cpp
+++ Others/K_num.cpp
@@ -1,6 +1,6 @@
 /********************************************************************************************
  * @author reddragon
- * @date 2020/11/5
+ * @date 2020/11/18
  * @brief easy 前K个数
  * 
  * 1. 堆排序
@@ -9,82 +9,88 @@
  * 循环K次就可以了
  * 
  * 时间复杂度：O(KlogN)        空间复杂度：O(1)
+ * 
+ * 2. 快速排序
+ * 快排每次可以确定一个数位置pos，看该pos和K差多少，然后缩小查找区间，
+ * 直到pos == K，输出
+ * 
+ * 时间复杂度: O(NlogN)         空间复杂度：O(N)
  ********************************************************************************************/
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using std::cin;
-using std::cout;
-using std::vector;
+using namespace std;
 
-// 找到数组的前K大个数，并且输出
-void K_num(vector<int> arr, int k)
+/* 快排的思路
+int findKth(vector<int> a, int n, int K)
 {
-    // 建大顶堆
-    int sz = arr.size();
-    for(int i = sz / 2 - 1; i >= 0; --i)
-    {// 从第一个非叶子结点开始
-        int root = i;
-        int max = root;
-        bool isExit = true;
-        do{// 下滤
-            isExit = true;
-            int left = root*2 + 1;
-            int right = root*2 + 2;
-            if(left < sz && arr[left] > arr[max])
-            {
-                max = left;
-                isExit = false;
-            }
-            if(right < sz && arr[right] > arr[max])
-            {
-                max = right;
-                isExit = false;
-            }
-            std::swap(arr[root],arr[max]);
-            root = max;
-        }while(!isExit);
-    }
-    while(k--)
+    // write code here
+    // 注意是逆序
+    srand(time(0));
+    int pos = rand() % n;
+    int low = 0, high = n - 1;
+    while (low < high)
     {
-        cout << arr[0] << std::endl;
-        std::swap(arr[0], arr[sz - 1]);
-        --sz;
-        // 恢复大顶堆
-        int max = 0;
-        int root = 0;
-        bool isExit = true;
-        do{// 下滤
-            isExit = true;
-            int left = root*2 + 1;
-            int right = root*2 + 2;
-            if(left < sz && arr[left] > arr[max])
-            {
-                max = left;
-                isExit = false;
-            }
-            if(right < sz && arr[right] > arr[max])
-            {
-                max = right;
-                isExit = false;
-            }
-            std::swap(arr[root],arr[max]);
-            root = max;
-        }while(!isExit);
+        while (low < pos && a[low] >= a[pos])
+            ++low;
+        swap(a[low], a[pos]);
+        pos = low;
+        while (pos < high && a[pos] > a[high])
+            --high;
+        swap(a[pos], a[high]);
+        pos = high;
     }
+    if (pos < K - 1)
+        return findKth(vector<int>(a.begin() + pos + 1, a.end()), n - pos - 1, K - pos - 1); // 确定的位置在pos左边
+    else if (pos > K - 1)
+        return findKth(vector<int>(a.begin(), a.begin() + pos), pos, K);
+    else
+        return a[pos];
+}
+ */
+void down(vector<int> &arr, int n, int pos)
+{
+    while (pos <= n / 2 - 1)
+    {
+        int left = pos * 2 + 1;
+        int right = pos * 2 + 2;
+        int maxPos = pos;
+        if (left < n && arr[left] > arr[maxPos])
+            maxPos = left;
+        if (right < n && arr[right] > arr[maxPos])
+            maxPos = right;
+        swap(arr[pos], arr[maxPos]);
+        // 不会再下滤了
+        if (pos == maxPos)
+            break;
+        else
+            pos = maxPos;
+    }
+    return;
+}
+
+int findKth(vector<int> a, int n, int K)
+{
+    // 从下往上做一次下滤
+    int pos = n / 2 - 1; // 最后一个非叶子节点
+    while (pos >= 0)
+    {
+        // 下滤操作
+        down(a, n, pos);
+        --pos;
+    }
+    for (int i = 1; i < K; ++i)
+    {
+        swap(a[0], a[n - 1]);
+        --n;
+        down(a, n, 0);
+    }
+    return a[0];
 }
 
 int main()
 {
-    int n;
-    while(cin >> n)
-    {
-        vector<int> arr(n);
-        for(int i = 0; i < n; ++i)
-            cin >> arr[i];
-        int k;
-        cin >> k;
-        K_num(arr, k);
-    }
+    vector<int> arr{1,2,3,4,5};
+    cout << findKth(arr, 5, 3) << endl;
     return 0;
 }
