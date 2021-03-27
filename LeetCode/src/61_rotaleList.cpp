d332bb2e244e8f7341e87cd0fc2e5ba012264adb
linglong <1730911462@qq.com>
2021-03-27 13:24:22 +0800

    二刷旋转链表

--- LeetCode/src/61_rotaleList.cpp
+++ LeetCode/src/61_rotaleList.cpp
@@ -1,10 +1,12 @@
 /*****************************************************************************
  * @author reddragon
- * @date 2020/7/23
- * @brief 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
- * 思路：先将链表变成循环链表。然后确定新的header
- * len - k%len - 1
- * 时间复杂度：O(N)  空间复杂度：O(1)
+ * @date 2021/3/27
+ * @brief 旋转链表 medium
+ * 
+ * 1.两次循环
+ * 先一次循环, 计算出链表长度, 并且把尾结点和头结点连接起来
+ * 然后head向前移动len - (len%k)步, 就是新的头结点
+ * 时间复杂度：O(N)         空间复杂度：O(1)
  *****************************************************************************/
 #include <iostream>
 using namespace std;
@@ -16,25 +18,28 @@ struct ListNode
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-ListNode *rotateRight(ListNode *head, int k)
+ListNode* rotateRight(ListNode* head, int k) 
 {
-    if (!head || k == 0 || !head->next)
+    if(head == nullptr || head->next == nullptr || k == 0)
         return head;
-    ListNode *p = head;
-    int len = 1;
-    while (p->next)
+    int len = 1;// 链表长度
+    // 找到尾结点
+    ListNode* tail = head;
+    while(tail->next)
     {
-        p = p->next;
-        len++;
+        tail = tail->next;
+        ++len;
     }
-    p->next = head; // 连接成循环链表
-    int num = len - k % len - 1;
-    //
-    while (num--)
+    // 变成循环链表
+    tail->next = head;
+    int i = len - (k % len);
+    while(--i)
+    {
         head = head->next;
-    p = head;
+    }
+    tail = head;
     head = head->next;
-    p->next = NULL;
+    tail->next = nullptr;
     return head;
 }
 
