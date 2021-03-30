c7cb3648cc6e2a2f09c6ac92d69838c26118d90c
lingFlya <1730911462@qq.com>
2021-03-31 00:33:07 +0800

    二分查找

--- LeetCode/src/74_searchMatrix.cpp
+++ LeetCode/src/74_searchMatrix.cpp
@@ -1,65 +1,69 @@
-/**********************************************
+/********************************************************************************************
  * @author reddragon
- * @date 2020/7/30
- * @brief 搜索二维矩阵 medium
- * 把二维数组视作一维数组，使用二分查找
- * 时间：O(logm*n) 空间：O(1)
- **********************************************/
+ * @date 2021/3/30
+ * @brief medium 搜索二叉矩阵
+ * 
+ * 1，循环
+ * 双重循环检查所有元素
+ * 时间复杂度: O(m*n)           空间复杂度: O(1)
+ * 
+ * 2. 二分查找
+ * 先写了个retVal函数将二维矩证的访问 变成 一维矩阵的访问
+ * 时间复杂度: O(logM*N)        空间复杂度: O(1)
+ ********************************************************************************************/
 #include <iostream>
 #include <vector>
-#include <string>
-#include <memory>
+#include <algorithm>
 using namespace std;
 
-// 把二维数组视作一维数组，使用二分查找
+inline int retVal(const vector<vector<int>> &matrix, int idx)
+{
+    int row = idx / matrix[0].size();
+    int col = idx % matrix[0].size();
+    return matrix[row][col];
+}
+
 bool searchMatrix(vector<vector<int>> &matrix, int target)
 {
-    size_t row = matrix.size();
-    if (row == 0)
-        return false;
-    size_t col = matrix[0].size();
-    int low = 0, high = row * col - 1;
-    int mid;
-    while (low <= high)
+    int sz = matrix.size() * matrix[0].size();
+    int low = 0, high = sz;
+    while(low < high)
     {
-        mid = (low + high) / 2;
-        int x = mid / col; // 第x行
-        int y = mid % col; // 第y列
-
-        if (matrix[x][y] > target)
+        int mid = (high - low) / 2 + low;
+        int val = retVal(matrix, mid);
+        if(val > target)
             high = mid - 1;
-        else if (matrix[x][y] < target)
+        else if(val < target)
             low = mid + 1;
         else
-            return true;
+            return true;      
     }
     return false;
 }
 
-// 暴力，全部遍历一遍
-// bool searchMatrix(vector<vector<int>> &matrix, int target)
-// {
-//     size_t row = matrix.size();
-//     if (row == 0)
-//         return false;
-//     size_t col = matrix[0].size();
-//     for (int i = 0; i < row; i++)
-//     {
-//         for (int j = 0; j < col; j++)
-//         {
-//             if (target == matrix[i][j])
-//                 return true;
-//         }
-//     }
-//     return false;
-// }
+/* 双重循环
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    for (int i = 0; i < matrix.size(); ++i)
+    {
+        for (int j = 0; j < matrix[i].size(); ++j)
+        {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+} */
 
 int main()
 {
     vector<vector<int>> matrix = {
         {1, 3, 5, 7},
         {10, 11, 16, 20},
-        {23, 30, 34, 50}};
+        {23, 30, 34, 50}
+    };
+    // for(int i = 0; i < 12; ++i)
+    //     cout << retVal(vec, i);
     cout << searchMatrix(matrix, 50);
     return 0;
 }
