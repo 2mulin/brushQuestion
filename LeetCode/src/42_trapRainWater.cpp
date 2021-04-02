d56875ad58bad069a307f3f4fbadd33365693d97
lingFlya <1730911462@qq.com>
2021-04-02 12:54:42 +0800

    双指针才是最优解

--- LeetCode/src/42_trapRainWater.cpp
+++ LeetCode/src/42_trapRainWater.cpp
@@ -1,72 +1,50 @@
-/***************************************************************************
- * @date 2020/7/11
+/********************************************************************************************
  * @author reddragon
- * @description: hard 第42题:接雨水
- * 以下两个算法都是每次计算一个height[i]所在的那一列所能接的雨水,而不是一个区域的算
- * 这也是我最开始没有思路的原因
- * 1. 暴力法：
- * 当前格子所在若是可以接雨水，那么此格子左右必然还有比它高的其他格子。
- * 取左右格子小的那一个高度与当前格子高度做减法，就可以算出当前格子所在列能够接到的
- * 雨水
- * 时间复杂度：O(n*n) 空间复杂度O(1)
+ * @date 2021/4/2
+ * @brief medium 接雨水
  * 
- * 2. 标记数组：
- * 使用left_max和right_max记录当前位置左或右边最大的高度。
- * 优化暴力法，每个都要搜索一次的缺陷。
- * 时间复杂度：O(n) 空间复杂度O(2*n)
- ****************************************************************************/
+ * 1，动态规划
+ * 一个点能够存水的要求就是左右两边都有比他高的点.
+ * 所以使用两个dp数组. 记录两边的高度的最大值.
+ * 最后一次遍历就完成了
+ * 前面这个数组可以优化,不需要, 用一个int表示就行了
+ * 时间复杂度: O(N)        空间复杂度: O(2N)
+ * 
+ * 2. 双指针
+ * 分别使用两个数表示左右两边的柱子当前值.如果leftMax < rightMax,
+ * 那么就以右边最大值为边界, 从左往右走
+ * 只要当前height[i] < leftMax, 那么这个位置就可以积水, 如果比leftMax大,那就更新leftMax
+ * 当leftMax > rightMax的时候, 那就以leftMax为边界, 从右往左走
+ * 只要当前height[i] > rightMax, 那么这个位置就可以积水.
+ * 时间复杂度: O(N)         空间复杂度: O(1)
+ ********************************************************************************************/
 #include <iostream>
 #include <vector>
-#include <string>
-#include <queue>
-#include <map>
-#include <algorithm>
-#include <cstring>
 using namespace std;
 
-// 优化
 int trap(vector<int> &height)
 {
-    if (height.size() <= 2)
+    int sz = height.size();
+    // 2个以下的元素, 无法存水
+    if(sz <= 2)
         return 0;
-    vector<int> left_max(height.size(), 0);  //表示当前位置左边最大的数
-    vector<int> right_max(height.size(), 0); //表示当前位置右边最大的数
-    for (int i = 1; i < left_max.size(); i++)
+    int leftMax = height[0];  // 一个数表示, 节省空间
+    vector<int> rightMax(sz, 0); // i ~ sz-i范围最高点
+    // 单调栈初始化
+    rightMax[sz - 1] = height[sz - 1];
+    for (int i = sz - 2; i >= 0; --i)
+        rightMax[i] = max(rightMax[i + 1], height[i]);
+    // 计算容量
+    int ret = 0;
+    for (int i = 1; i < sz - 1; ++i)
     {
-        left_max[i] = max(height[i - 1], left_max[i - 1]);
+        leftMax = max(leftMax, height[i]);
+        int minHigh = min(leftMax, rightMax[i]);
+        if (height[i] < minHigh)
+            ret += (minHigh - height[i]);
     }
-    for (int i = right_max.size() - 2; i >= 0; i--)
-    {
-        right_max[i] = max(height[i + 1], right_max[i + 1]);
-    }
-
-    int ans = 0;
-    for (int i = 1; i < height.size() - 1; i++)
-    {
-        // 左右都比当前高，才会盛水
-        if (left_max[i] > height[i] && right_max[i] > height[i])
-            ans += min(left_max[i], right_max[i]) - height[i];
-    }
-    return ans;
+    return ret;
 }
-
-// 暴力 时间O(n*n) 空间O(1)
-// int trap(vector<int> &height)
-// {
-//     int ans = 0;
-//     for (int i = 1; i < height.size() - 1; i++)
-//     {
-//         int left_max = 0, right_max = 0;
-//         for (int j = 0; j < i; j++)
-//             left_max = max(left_max, height[j]);
-//         for (int j = i + 1; j < height.size(); j++)
-//             right_max = max(right_max, height[j]);
-//         // 左右都比当前高，才会盛水
-//         if (left_max > height[i] && right_max > height[i])
-//             ans += min(left_max, right_max) - height[i];
-//     }
-//     return ans;
-// }
 
 int main()
 {
