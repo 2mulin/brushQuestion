9a6a26e87ac217fa8d7f6fca953b943107e2f472
lingFlya <1730911462@qq.com>
2021-04-06 11:02:34 +0800

    双指针(数组要记得判断是否越界)

--- LeetCode/src/80_removeDuplicates.cpp
+++ LeetCode/src/80_removeDuplicates.cpp
@@ -1,16 +1,44 @@
 /*******************************************************************************
  * @author reddragon
- * @date 2020/8/7
- * @brief 删除数组中的重复项2 medium
+ * @date 2021/4/6
+ * @brief medium 删除数组中的重复项2
+ * 
+ * 1. 双指针
  * 每个数字最多重复2次
- * 双指针法：一个指针指向当时满足条件的位置，一个指向需要继续测试的位置。
- * 同时需要计数，因为每个数字可以重复两次。
+ * 一个指针指向当时满足条件的位置，一个指向需要继续测试的位置。
+ * 同时需要计数，因为每个数字可以重复两次。最开始的思路就是
+ * 边赋值便记录已经相等元素的个数。等到超过2的时候，就把后面的全部删除。
+ * 时间复杂度： O(N)        空间复杂度：O(1)
  *******************************************************************************/
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// 二刷
 int removeDuplicates(vector<int> &nums)
+{
+    size_t i = 0, j = 0;
+    while (j < nums.size())
+    {
+        if (j + 1 < nums.size() && nums[j] == nums[j + 1])
+        {
+            int tmp = nums[j];
+            j += 2;
+            while (j < nums.size() && nums[j] == tmp)
+                ++j;
+            nums[i++] = tmp;
+            nums[i++] = tmp;
+            if (j < nums.size())
+                nums[i] = nums[j];
+            continue;
+        }
+        nums[i++] = nums[j++];
+    }
+    return nums.size() - (j - i);
+}
+
+// 2020年8月
+/* int removeDuplicates(vector<int> &nums)
 {
     int len = 0;
     int count = 0;
@@ -35,7 +63,7 @@ int removeDuplicates(vector<int> &nums)
     }
     return len;
 }
-
+ */
 int main()
 {
     vector<int> arr{0, 0, 1, 1, 1, 1, 2, 3, 3};
