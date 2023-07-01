d1a97cc8afbdbe3f0e52121fbc09605340f4cb6b
2mu <1730911462@qq.com>
2023-07-01 17:11:32 +0800

    :recycle: redo: 重刷一遍, 还是能学到一些优美的代码写法

--- LeetCode/src/1_twoSum.cpp
+++ LeetCode/src/1_twoSum.cpp
@@ -1,42 +1,60 @@
-/****************************************************************************************************
- * @date 2020/6/10
- * @author reddragon
+/**
+ * @date 2023/7/1
+ * @author 2mu
  * @brief easy 两数之和
+ *
+ * 1. 哈希表
+ * 重新刷一遍
  * 
- * 1. 暴力法：遍历每个元素 xx，并查找是否存在一个值与 target - xtarget−x 相等的目标元素。
- * 时间复杂度：O(n2).空间复杂度：O(1)
+ * 如果考虑有重复元素这一点, 还是比较难的, 因为如果3个重复元素, 则可以生成多种index的序列;
+ * 但是题目给了限制, 只有一个答案; 所以就好做多了;
  * 
- * 2. 哈希表：一个简单的实现使用了两次迭代。在第一次迭代中，我们将每个元素的值和它的索引添加到表中。
- * 然后，在第二次迭代中，我们将检查每个元素所对应的目标元素(target - nums[i]target−nums[i])是否存在于表中。
- * 注意，该目标元素不能是 nums[i]nums[i] 本身！
-****************************************************************************************************/
+ * 官方题解给的程序更加棒, 因为它是遍历数组时, 将元素一个个加进哈希表的, 就不需要判断是否重复元素满足条件的情况; 
+ * 
+ * 代码更加高效, 优美; 看我下次不看题解能不能想到这种优美写法;
+ * 
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(m)
+ */
+
 #include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
-#include <iterator>
+#include <unordered_map>
+
 using namespace std;
 
 vector<int> twoSum(vector<int> &nums, int target)
 {
-    vector<int> ans(2);
-    int size = static_cast<int>(nums.size());
-    if (size <= 1)
-        return ans;
-    multimap<int, int> m;
-    for (int i = 0; i < size; i++)
-        // 第一个是键，键可以重复，值不可以重复
-        m.insert(pair<int, int>{nums[i], i});
-    for (auto i = m.begin(); i != m.end(); i++)
+    std::unordered_multimap<int, int> hashMap;
+    for(int i = 0; i < nums.size(); ++i)
     {
-        auto num = m.find(target - i->first); // 根据key找，看存不存在，返回迭代器
-        if (num != m.end())
+        hashMap.insert({nums[i], i});
+    }
+
+    std::vector<int> ans;
+    for(auto iter = hashMap.begin(); iter != hashMap.end(); ++iter)
+    {
+        int diff = target - iter->first;
+        auto iter2 = hashMap.find(diff);
+        if(iter2 == hashMap.end())
+            continue;
+        
+        if(iter2 != iter)
         {
-            // 8 = 4 + 4但是数组里只有一个4
-            if (num != i || (num == i && (++num)->first == i->first))
+            ans.push_back(iter->second);
+            ans.push_back(iter2->second);
+            return ans;
+        }
+        else
+        {
+            // 题目说只有一个答案, 所以只最多两个相同元素
+            if(hashMap.count(diff) == 2)
             {
-                ans[0] = i->second; // 保存value
-                ans[1] = num->second;
+                auto range = hashMap.equal_range(diff);
+                for(auto it = range.first; it != range.second; ++it)
+                {
+                    ans.push_back(it->second);
+                }
                 return ans;
             }
         }
@@ -46,9 +64,16 @@ vector<int> twoSum(vector<int> &nums, int target)
 
 int main()
 {
-    vector<int> nums{-3, 4, 3, 90};
-    vector<int> ans = twoSum(nums, 7);
-    for (auto i : ans)
-        cout << i << '\t';
+    std::vector<int> nums = {2,7,11,15};
+    std::vector<int> ans = twoSum(nums, 9);
+    for(auto pos : ans)
+        std::cout << pos << ' ';
+    std::cout << std::endl;
+
+    nums = {3, 3};
+    ans = twoSum(nums, 6);
+    for(auto pos : ans)
+        std::cout << pos << ' ';
+    std::cout << std::endl;
     return 0;
 }
