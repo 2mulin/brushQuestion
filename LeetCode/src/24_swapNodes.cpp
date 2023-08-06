da7862cf7fe99183f90ceea9870c9878d98bafe2
2mu <1730911462@qq.com>
2023-08-06 11:07:18 +0800

    :sparkle: feat: 链表操作, 递归或者迭代

--- LeetCode/src/24_swapNodes.cpp
+++ LeetCode/src/24_swapNodes.cpp
@@ -1,53 +1,73 @@
-/*************************************************
- * @date 2020/6/19
- * @author reddragon
- * @description: medium，两两交换链表结点
- * 三个指针，迭代无脑换。注意细节就好了
- * 时间复杂度：O(N)   空间复杂度：O(1)
- ************************************************/
+/**
+ * @date 2023/8/6
+ * @author 2mu
+ * @brief medium 两两交换链表中的节点 
+ * 
+ * 1. 迭代
+ * 一般链表操作都是两种, 递归或者迭代; 递归写出来代码优雅易懂, 但是空间复杂度高;
+ * 迭代写出来代码比较难懂, 但是占用空间相对较少;
+ * 
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(1)
+ */
+
 #include <iostream>
-#include <vector>
-#include <cstring>
-#include <algorithm>
+
 using namespace std;
 
-struct ListNode
-{
+struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+// 递归实现
+// istNode *swapPairs(ListNode *head)
+// {
+//     if(head == nullptr || head->next == nullptr)
+//         return head;
+    
+//     ListNode* newHead = head->next;
+//     head->next = swapPairs(newHead->next);
+//     newHead->next = head;
+//     return newHead;
+// }
+
 
 ListNode *swapPairs(ListNode *head)
 {
-    // 如果是0或1个结点
-    if (!head || !head->next)
+    if(head == nullptr || head->next == nullptr)
         return head;
-    // 两个结点以上
-    ListNode *ans = head->next, *pre = nullptr;
-    ListNode *p1 = head, *p2 = head->next;
-    while (p1 && p2)
+
+    ListNode* newList = new ListNode(-1, head->next); // 头节点;
+
+    ListNode* p = head;
+    ListNode* curr = head, *next = head->next;
+    while(next)
     {
-        // 交换
-        p1->next = p2->next;
-        p2->next = p1;
-        if (pre)
-            pre->next = p2;
-        // 前进
-        pre = p1;
-        p1 = p1->next;
-        if (!p1)
-            break;
-        p2 = p1->next;
+        p = next->next;
+
+        next->next = curr;
+        if(p && p->next)
+            curr->next = p->next;
+        else
+            curr->next = p;
+
+        curr = p;
+        if(p)
+            next = p->next;
+        else
+            next = NULL;
     }
-    return ans;
+    p = newList->next;
+    delete newList;
+    return p;
 }
+
 
 int main()
 {
-    ListNode *p = new ListNode(1);
-    p->next = new ListNode(2);
-    p->next->next = new ListNode(3);
-
     return 0;
 }
