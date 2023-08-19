a8a69b5a32eccb40adc3c970929d476849c5c135
2mu <1730911462@qq.com>
2023-08-19 20:52:43 +0800

    :sparkles: feat: 愿世界上所有的代码都这么简单

---
+++ LeetCode/src/2235_sum.cpp
@@ -0,0 +1,25 @@
+/**
+ * @date 2023/8/19
+ * @author 2mu
+ * @brief easy 两整数相加 
+ * 
+ * 1. 过于简单
+ * 
+ * 
+ * 时间复杂度: O(1)
+ * 空间复杂度: O(1)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int main()
+{
+    return 0;
+}
