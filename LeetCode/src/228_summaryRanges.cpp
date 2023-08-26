c0554a16cd05cb4b89b004d5b3fc7366c82f62d9
2mu <1730911462@qq.com>
2023-08-26 23:38:59 +0800

    :sparkle: feat: 记住分组循环的写法, 简单易懂,避免判断边界条件

--- LeetCode/src/228_summaryRanges.cpp
+++ LeetCode/src/228_summaryRanges.cpp
@@ -1,50 +1,81 @@
-/********************************************************************************************
- * @author reddragon
- * @date 2021/1/10
+/**
+ * @date 2023/8/26
+ * @author 2mu
  * @brief easy 汇总区间
+ *
+ * 1. 双指针 + 遍历
+ * 首先题目保证给定的数组中没有重复的数字, 并且是按照升序排列;
+ * 所以直接遍历是合理的, 也就是双指针 记录区间位置, 遍历时如果发现当前位置
+ * 的元素 - 上一个位置的元素不等于1时, 表示不连续; 
  * 
- * 1，遍历一遍
- * 遍历一遍，找到所有连续的区间，并且记录下来
- * 时间复杂度：O(n)
- ********************************************************************************************/
+ * 这题目恶心人的地方在于 处理边界条件, 以及计算越界的问题;
+ * 
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(1)
+ * 
+ * 2. 分组循环
+ * 使用两个循环的写法, 时间复杂度并没有增加; 而且代码更简洁易懂, 不用处理边界条件;
+ * 
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(1)
+ */
+
 #include <iostream>
 #include <vector>
+#include <string>
+
 using namespace std;
 
 vector<string> summaryRanges(vector<int> &nums)
 {
-    vector<string> ret;
-    if (nums.empty())
-        return ret;
-    int start = nums[0], end = nums[0];
-    for (size_t i = 1; i <= nums.size(); ++i)
+    std::vector<string> vctAns;
+    if(nums.empty())
+        return vctAns;
+    if(nums.size() == 1)
     {
-        if (i < nums.size() && nums[i] == end + 1)
-            end = nums[i];
+        vctAns.push_back(std::to_string(nums.front()));
+        return vctAns;
+    }
+    int x = nums.front(), y = nums.front();
+    for(size_t i = 1; i < nums.size(); ++i)
+    {
+        if( i != nums.size() - 1)
+        {
+            if(nums[i] != y + 1)
+            {
+                if(x == y)
+                    vctAns.push_back(std::to_string(x));
+                else
+                    vctAns.push_back(std::to_string(x) + "->" + std::to_string(y));
+                x = nums[i];
+            }
+            y = nums[i];
+        }
         else
         {
-            string tmp;
-            tmp += to_string(start);
-            if (start != end)
+            if(nums[i] != y + 1)
             {
-                tmp.push_back('-');
-                tmp.push_back('>');
-                tmp += to_string(end);
+                if(x == y)
+                    vctAns.push_back(std::to_string(x));
+                else
+                    vctAns.push_back(std::to_string(x) + "->" + std::to_string(y));
+                vctAns.push_back(std::to_string(nums[i]));
             }
-            ret.push_back(tmp);
-            // 防止内存泄漏
-            //if(i < nums.size())
-                start = end = nums[i];
+            else
+            {
+                y = nums[i];
+                vctAns.push_back(std::to_string(x) + "->" + std::to_string(y));
+            }
         }
     }
-    return ret;
+    return vctAns;
 }
 
 int main()
 {
-    vector<int> arr = {-1};
-    vector<string> ret = summaryRanges(arr);
-    for(auto i : ret)
-        cout << i << endl;
+    std::vector<int> vct = {-2147483648,-2147483647,2147483647};
+    std::vector<string> vctAns = summaryRanges(vct);
+    for(auto str : vctAns)
+        std::cout << str << std::endl;
     return 0;
 }
