eaf78265d11630314fa8527966ba71b77eddf994
2mu <1730911462@qq.com>
2023-08-27 12:53:01 +0800

    :recycle: refactor: 刷第二次, 注意sort的使用方式

--- LeetCode/src/56_mergeIntervals.cpp
+++ LeetCode/src/56_mergeIntervals.cpp
@@ -1,51 +1,62 @@
-/************************************************************
- * @date 2020/7/19
- * @author reddragon
- * @brief 合并区间 medium
- * 先将数组排序，然后依次判断相邻的区间是否可以合并。
- * 主要是排序的时间，合并是O(n)
- * 时间复杂度：O(nlogn)  空间复杂度：O(n)
-************************************************************/
+/**
+ * @date 2023/8/27
+ * @author 2mu
+ * @brief medium 合并区间
+ *
+ * 1. 排序+遍历
+ * 先排序, 因为题目给出的要求并没有保证输入是按照顺序排列的, 直接按照每个区间的begin进行排序就行
+ * 排序之后, 则开始遍历, 一旦发现遍历到的当前区间 begin <= 上一个区间的end, 则可以合并;
+ * 
+ * 时间复杂度: O(nlogn)
+ * 空间复杂度: O(n)
+ */
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
 
 vector<vector<int>> merge(vector<vector<int>> &intervals)
 {
-    if (intervals.size() <= 1)
-        return intervals;
-    sort(intervals.begin(), intervals.end());
+    std::vector<std::vector<int>> vctAns;
+    if(intervals.empty())
+        return vctAns;
 
-    vector<vector<int>> ans;
-    for (auto it = intervals.begin(); it != intervals.end() - 1; it++)
+    // vector的比较, 默认按照元素字典序 排序; (不知道为啥用自己写的lambda表达式会导致内存泄漏, leetcode检测不通过)
+    // std::sort(intervals.begin(), intervals.end(), [](std::vector<int>& a, std::vector<int>& b)->bool{
+    //     if(a.empty() || b.empty())
+    //         return true;
+    //     if(a.front() <= b.front())
+    //         return true;
+    //     return false;
+    // });
+    std::sort(intervals.begin(), intervals.end());
+    vctAns.push_back(intervals.front());
+    for(int i = 1; i < intervals.size(); ++i)
     {
-        // 当前可能需要合并的区间
-        int L = it->operator[](0);
-        int R = it->operator[](1);
-        if (ans.empty() || ans.back().operator[](1) < L)
-            ans.push_back(*it);
+        int prev_end = vctAns.back()[1], curr_begin = intervals[i][0];
+        if(prev_end >= curr_begin)
+        {
+            // 可以合并区间
+            vctAns.back()[1] = std::max(prev_end, intervals[i][1]);
+        }
         else
         {
-            // vector<int> temp(2);
-            // temp[0] = ans.back()[0];
-            // 确定右边界
-            // temp[1] = max(R,ans.back()[1]);
-            // ans.back() = temp;
-            // 通过观察上面的代码，实际上只是替换了ans最后元素的右边界值
-            ans.back()[1] = max(R, ans.back()[1]);
+            // 不可以合并
+            vctAns.push_back(intervals[i]);
         }
     }
-    return ans;
+    return vctAns;
 }
 
 int main()
 {
-    vector<vector<int>> arr;
-    for (int i = 0; i < 4; i++)
+    std::vector<std::vector<int>> intervals{{1,3},{2,6},{8,10},{15,18}};
+    std::vector<std::vector<int>> vctAns = merge(intervals);
+    for(auto interval : vctAns)
     {
-        vector<int> temp(2, rand() % 10);
-        arr.push_back(temp);
+        std::cout << "[" << interval[0] << ", " << interval[1] << "]" << std::endl;
     }
     return 0;
 }
