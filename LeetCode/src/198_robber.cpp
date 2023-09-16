3465365de5f6bc0eb6fb95ac615668d0580b1ac1
2mu <1730911462@qq.com>
2023-09-16 21:23:32 +0800

    :sparkle: feat: 重新刷一遍, dp状态转移方程要多多思考...

--- LeetCode/src/198_robber.cpp
+++ LeetCode/src/198_robber.cpp
@@ -1,49 +1,55 @@
-/********************************************************************************************
- * @author reddragon
- * @date 2021/1/23
+/**
+ * @date 2023/9/16
+ * @author 2mu
  * @brief medium 打家劫舍
+ *
+ * 1. dp
+ * 印象中已经做过这一题了, 再做一次; 要求是不能偷两个相邻的房间;
+ * 假设第i个房间的现金数是 nums[i], 假设nums数组长度为len, 
+ * 再设置一个dp数组, dp[i]表示nums长度为i + 1(<= len)时, 且**nums[i]必偷**, 最多能偷到的现金数;
  * 
- * 1，动态规划
- * 首先分析一下，相邻的只能偷一个。
- * 使用一个dp数组，dp[i]表示偷到第i个房子的时候最多能偷盗的钱财总和。
- * 可以得到一个状态转移方程：dp[i] = max(dp[i - 1], dp[i - 2] + 1);
- * 时间复杂度：O(n)			空间复杂度：O(n)
+ * 状态转化: i=0, dp[i] = nums[0]
+ *          i=1, dp[i] = nums[1]
+ *          i=2, dp[i] = nums[0] + nums[i]
+ *          i>=3, dp[i] = max(dp[i-3] + nums[i], dp[i-2] + nums[i])
  * 
- * 2. 滚动数组
- * 仙儿空间可以优化，使用滚动数组就行了
- ********************************************************************************************/
+ * 最终答案就是 max(dp[len - 1], dp[len - 2], dp[len - 3])
+ * 
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
+ * 
+ * 2. 官方题解 dp
+ * 
+ * 官方题解dp思路更加清晰, 看我下次能否想起来 所以说dp写得好不好, 就看状态解析方程好不好...
+ */
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 int rob(vector<int> &nums)
 {
-	int size = nums.size();
-	if (size == 0)
-		return 0;
-	else if (size == 1)
-		return nums[0];
-	else if (size == 2)
-		return max(nums[0], nums[1]);
-	else
-	{
-		vector<int> dp(2);
-		dp[0] = nums[0];
-		dp[1] = max(nums[0], nums[1]);
-		int be = 0, ed = 1;
-		for (int i = 2; i < size; ++i)
-		{
-			dp[be] = max(dp[ed], dp[be] + nums[i]);
-			swap(be, ed);
-		}
-		return max(dp[0], dp[1]);
-	}
+    int len = nums.size();
+    if(len == 1)
+        return nums[0];
+    if(len == 2)
+        return max(nums[0], nums[1]);
+    std::vector<int> dp(len);
+    dp[0] = nums[0];
+    dp[1] = nums[1];
+    dp[2] = nums[0] + nums[2];
+    for(int i = 3; i < len; ++i)
+    {
+        dp[i] = std::max(dp[i - 3] + nums[i], dp[i - 2] + nums[i]);
+    }
+    int ans = std::max(dp[len - 1], dp[len - 2]);
+    ans = std::max(ans, dp[len - 3]);
+    return ans;
 }
 
 int main()
 {
-	vector<int> arr{1, 2 ,3, 2, 4};
-	cout << rob(arr) << endl;
-	return 0;
+
+    return 0;
 }
