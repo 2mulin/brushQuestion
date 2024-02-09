9f78f07bea68ab4644ac8309862af0f2109d56cd
2mu <1730911462@qq.com>
2024-02-09 23:52:33 +0800

    :sparkle: feat: 中等题, 还是Tree遍历

--- LeetCode/src/236_lowestCommonAncestor.cpp
+++ LeetCode/src/236_lowestCommonAncestor.cpp
@@ -1,57 +1,123 @@
-/********************************************************************************************
- * @author reddragon
- * @date 2021/2/3
- * @brief medium 最近公共祖先
+/**
+ * @date 2024/2/9
+ * @author 2mu
+ * @brief medium 二叉树的最近公共祖先
  * 
- * 1.将路径暂存
- * 题目保证相同的结点不会出现两次，所以我们利用一个数组存储root到指定p、q的路径
- * 最终判断两条路径的最后一个相同点，那就是最近公共祖先。
- * 时间复杂度：O(n)		空间复杂度：O(n)
+ * 1. DFS(将路径暂存)
+ * 由于是二叉树, 所以root节点到指定节点的路径只存在一条; 现在需要求公共祖先节点;
+ * 可以转换为求路径上的同时出现的节点; 那只要先把节点的路径求出来即可; 然后求两条路径上
+ * 都出现的节点;
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
  * 
  * 官方题解的想法是利用map存下父节点和子节点的关系，这样就可以通过子节点找到父节点了
- * 
- * 2.递归
- * 如果x的左右子树，包含p、q结点，那么x可能是我们要找的，以此为条件递归
  * 
  ********************************************************************************************/
 #include <iostream>
 #include <vector>
 #include <stack>
-#include <dataStruct.h>
+
 using namespace std;
 
-bool findPath(TreeNode *root, TreeNode *node, vector<TreeNode *> &path)
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 递归解法, 还是非常不错的...
+// bool findPath(TreeNode *root, TreeNode *node, vector<TreeNode *> &path)
+// {
+//     if (root == nullptr)
+//         return false;
+//     path.push_back(root);
+//     if (root == node)
+//         return true;
+//     if (findPath(root->left, node, path))
+//         return true;
+//     else
+//     {
+//         while (path.back() != root)
+//             path.pop_back(); // 回溯
+//         if (findPath(root->right, node, path))
+//             return true;
+//     }
+//     return false;
+// }
+
+// TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+// {
+//     // 得到root-》p和root-》q的两条路径
+//     vector<TreeNode *> vecP, vecQ;
+//     findPath(root, p, vecP);
+//     findPath(root, q, vecQ);
+//     int i = 0;
+//     int len = min(vecP.size(), vecQ.size());
+//     if (len == 0)
+//         return nullptr;
+//     while (i < len && vecP[i] == vecQ[i])
+//         ++i;
+//     return vecP[i - 1];
+// }
+
+
+// 非递归搜索路径
+std::vector<TreeNode*> getSearchPath(TreeNode* root, TreeNode* target)
 {
-    if (root == nullptr)
-        return false;
-    path.push_back(root);
-    if (root == node)
-        return true;
-    if (findPath(root->left, node, path))
-        return true;
-    else
+    std::vector<TreeNode*> vct;
+    vct.push_back(root);
+    if(root == target)
+        return vct;
+
+    while(!vct.empty())
     {
-        while (path.back() != root)
-            path.pop_back(); // 回溯
-        if (findPath(root->right, node, path))
-            return true;
+        TreeNode* node = vct.back();
+        
+        if (node->left)
+        {
+            vct.push_back(node->left);
+        }
+        else if(node->right)
+        {
+            vct.push_back(node->right);
+        }
+        else
+        {
+            TreeNode* tmp = node;
+            do
+            {
+                tmp = node;
+                vct.pop_back();
+                if(vct.empty())
+                    break;// 按照题目来说, 路径一定存在, 不可能出现搜索不到的情况....
+                node = vct.back();
+            }while(!node->right || node->right == tmp);
+            vct.push_back(node->right);
+        }
+        if(vct.back() == target)
+            return vct;
     }
-    return false;
+    return {};
 }
 
 TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
 {
-    // 得到root-》p和root-》q的两条路径
-    vector<TreeNode *> vecP, vecQ;
-    findPath(root, p, vecP);
-    findPath(root, q, vecQ);
-    int i = 0;
-    int len = min(vecP.size(), vecQ.size());
-    if (len == 0)
-        return nullptr;
-    while (i < len && vecP[i] == vecQ[i])
-        ++i;
-    return vecP[i - 1];
+    std::vector<TreeNode*> vct1 = getSearchPath(root, p);
+    std::vector<TreeNode*> vct2 = getSearchPath(root, q);
+    // 前半部分祖先节点一定是相同的...
+    int len = min(vct1.size(), vct2.size());
+    for(int i = 0; i < len; ++i)
+    {
+        if(vct2[i] != vct1[i])
+        {
+            return vct1[i - 1];
+        }
+    }
+    return vct1[len - 1];
 }
 
 int main()
