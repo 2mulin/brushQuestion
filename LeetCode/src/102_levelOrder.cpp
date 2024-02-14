a2bfebf5388de391170066a441c99b9ac1861692
2mu <1730911462@qq.com>
2024-02-14 11:10:35 +0800

    :sparkle: feat: 又到每日一题了, 二刷

--- LeetCode/src/102_levelOrder.cpp
+++ LeetCode/src/102_levelOrder.cpp
@@ -1,50 +1,59 @@
-/*******************************************************************************************
- * @author reddragon
- * @date 2020/8/29
- * @brief easy 树的层次遍历
- * 这个有点不一样，他是要求一层一层的输出，而不是整个输出
+/**
+ * @date 2024/2/14
+ * @author 2mu
+ * @brief medium 二叉树的层次遍历
  * 
- * 还是一样的思路：队列作为辅助结构。
- * 时间复杂度:O(N)         空间复杂度:O(N)
- *******************************************************************************************/
+ * 1. 利用队列
+ * 利用队列进行层次遍历; 额外使用一个变量记录每层的节点数量; 就可以完成分层;
+ * 
+ * 时间复杂度:O(N)
+ * 空间复杂度:O(N)
+ */
 #include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
-
-struct TreeNode
-{
+struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+using namespace std;
 
 vector<vector<int>> levelOrder(TreeNode *root)
 {
-    vector<vector<int>> ans;
-    if (root == NULL)
-        return ans;
-    queue<TreeNode *> qu;
-    qu.push(root);
-    while (!qu.empty())
+    std::vector<vector<int>> ans;
+    std::queue<TreeNode*> que;
+    if(root)
+        que.push(root);
+
+    std::vector<int> level;// 一层节点
+    int level_node_count = 1;
+    while(!que.empty())
     {
-        vector<int> temp;
-        size_t len = qu.size();// 得到这一层的元素个数
-        for (size_t i = 0; i < len; i++)
+        TreeNode* node = que.front();
+        que.pop();
+        level.push_back(node->val);
+        --level_node_count;
+
+        if(node->left)
+            que.push(node->left);
+        if(node->right)
+            que.push(node->right);
+        if(level_node_count == 0)
         {
-            TreeNode *p = qu.front();
-            qu.pop();
-            temp.push_back(p->val);
-            if (p->left)
-                qu.push(p->left);
-            if (p->right)
-                qu.push(p->right);
+            level_node_count = que.size();
+            ans.push_back(level);
+            level.clear();
         }
-        ans.push_back(temp);
     }
     return ans;
 }
+
 
 int main()
 {
