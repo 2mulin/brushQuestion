6fddd2be897a365890c05141971a42b2f40a910f
2mu <1730911462@qq.com>
2024-04-14 11:44:54 +0800

    :sparkle: feat: 回顾二叉搜索树的性质

--- LeetCode/src/783_diff.cpp
+++ LeetCode/src/783_diff.cpp
@@ -1,20 +1,23 @@
-/********************************************************************************************
- * @author reddragon
- * @date 2021/4/14
- * @brief medium 搜索二叉树的最小差值
+/**
+ * @author mulin
+ * @date 2024/4/14
+ * @brief easy 搜索二叉树的最小距离
  * 
- * 1，性质
- * 首先一定要想得起，搜索二叉树的性质，也就是搜索二叉树的中序遍历是
- * 一个有序的数列。
- * 那么最简单的思路就有了，那就是中序遍历得到有序数列，
- * 在有序数列中找到差值最小就很简单了。
- * 时间复杂度： O(n)         空间复杂度：O(n)
- ********************************************************************************************/
+ * 1. 搜索二叉树性质
+ * 一定要先想起搜索二叉树的性质, 也就是搜索二叉树的中序遍历是有序的;
+ * 所以先中序遍历得到一个有序数组, 然后遍历数组, 每相邻两个元素做差, 记录差的最小值;
+ * 
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
+ */
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <climits>
+
 using namespace std;
+
 
 struct TreeNode {
     int val;
@@ -25,26 +28,27 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-vector<int> arr;
 
-void midTrav(TreeNode *root)
+void mid_traversal(TreeNode *root, std::vector<int>& sorted_array)
 {
     if (root == nullptr)
         return;
-    midTrav(root->left);
-    arr.push_back(root->val);
-    midTrav(root->right);
+    mid_traversal(root->left, sorted_array);
+    sorted_array.push_back(root->val);
+    mid_traversal(root->right, sorted_array);
 }
 
 int minDiffInBST(TreeNode *root)
 {
+    std::vector<int> sorted_array;
     // 中序遍历获得有序数列
-    midTrav(root);
+    mid_traversal(root, sorted_array);
     int ans = INT_MAX;
-    for (int i = 1; i < arr.size(); ++i)
-        ans = min(ans, abs(arr[i] - arr[i - 1]));
+    for (int i = 1; i < sorted_array.size(); ++i)
+        ans = min(ans, abs(sorted_array[i] - sorted_array[i - 1]));
     return ans;
 }
+
 
 int main()
 {
