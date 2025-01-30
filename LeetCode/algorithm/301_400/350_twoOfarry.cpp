e362fe07f62c0e59dfcd33e92c0a4f8166b8a460
2mu <1730911462@qq.com>
2025-01-30 21:04:23 +0800

    :recycle: refactor: 重刷一遍

--- LeetCode/algorithm/301_400/350_twoOfarry.cpp
+++ LeetCode/algorithm/301_400/350_twoOfarry.cpp
@@ -1,108 +1,63 @@
-/***************************************************************************
- * @date 2020/7/13
- * @author reddragon
- * @brief hard 第350题:两个数组的交集
+/**
+ * @date 2025/1/30
+ * @author 2mu 
+ * @brief easy 两个数组的交集II
+ * 
  * 1. 哈希表
- * 哈希表记录一个数组，num出现的次数，num作为key，count作为value
- * 在遍历另一个数组，作比较。
- * 时间复杂度：O(n+m)  空间复杂度：O(n)
+ * 对两个数组取交集，需要注意的是如果同一个数字在数组中出现多次，则交集中也要出现多次。
+ * 所以并不是普通的交集，要考虑数字出现次数。这里使用哈希表记录数字出现次数。
  * 
- * 2. 先排序，双指针一次比较
- * 时间复杂度：O(nlogn + mlonm) 空间复杂度：O(n)
+ * 这里的数组长度小于1000，数组元素大小小于1000。可以考虑直接使用桶计数就行。
  * 
- * 3. stl
- * 好用的一批
- ****************************************************************************/
+ * 时间复杂度：O(n)  空间复杂度：O(n)
+ * 
+ * 2. 排序 + 双指针
+ * 很有意思的思路，代码会更加清晰。
+ * 
+ * 时间复杂度：O(nlogn)  空间复杂度：O(n)
+ */
+
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
+
 using namespace std;
 
-// 哈希表记录num出现的次数，num作为key，count作为value
+
 vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
 {
-    vector<int> ans;
-    unordered_map<int, int> hash;
-    if (nums1.size() <= nums2.size())
+    std:vector<int> bucket1(1001, 0), bucket2(1001, 0);
+    for(int i = 0; i < nums1.size(); ++i)
     {
-        for (int i = 0; i < nums1.size(); i++)
+        int num = nums1[i];
+        ++bucket1[num];
+    }
+    
+    for(int i = 0; i < nums2.size(); ++i)
+    {
+        int num = nums2[i];
+        ++bucket2[num];
+    }
+
+    std::vector<int> result;
+    for(int i = 0; i < 1001; ++i)
+    {
+        int min_size = std::min(bucket1[i], bucket2[i]);
+        while (min_size >= 1)
         {
-            hash[nums1[i]]++; //计数
-        }
-        for (int i = 0; i < nums2.size(); i++)
-        {
-            if (hash.find(nums2[i]) != hash.end() && hash[nums2[i]] != 0)
-            {
-                ans.push_back(nums2[i]);
-                hash[nums2[i]]--;
-            }
+            result.push_back(i);
+            --min_size;
         }
     }
-    else
-    {
-        for (int i = 0; i < nums2.size(); i++)
-        {
-            hash[nums2[i]]++;
-        }
-        for (int i = 0; i < nums1.size(); i++)
-        {
-            if (hash.find(nums1[i]) != hash.end() && hash[nums1[i]] != 0)
-            {
-                ans.push_back(nums1[i]);
-                hash[nums1[i]]--;
-            }
-        }
-    }
-    return ans;
+    return result;
 }
 
-/* C++自带算法求交集：std::set_intersection()
-vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
-{
-    vector<int> ans;
-    sort(nums1.begin(), nums1.end());
-    sort(nums2.begin(), nums2.end());
-    std::set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(ans));
-    return ans;
-}
-*/
-
-/* 暴力，只要用在排序了  时间复杂度：O(nlogn) 空间复杂度：O(n)
-vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
-{
-    vector<int> ans;
-    sort(nums1.begin(), nums1.end());
-    sort(nums2.begin(), nums2.end());
-    int i = 0, j = 0;
-    while (i < nums1.size() && j < nums2.size())
-    {
-        if (nums1[i] == nums2[j])
-        {
-            ans.push_back(nums2[j]);
-            i++;
-            j++;
-            continue;
-        }
-        else if (nums1[i] > nums2[j])
-        {
-            j++;
-        }
-        else
-            i++;
-    }
-    return ans;
-}
-*/
 
 int main()
 {
-    vector<int> nums1{1, 2, 3};
-    vector<int> nums2{2, 2, 3, 6};
-    vector<int> ans = intersect(nums1, nums2);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << '\t';
-    }
+    std::vector<int> nums1{1, 2, 2, 1}, nums2{2, 2};
+    std::vector<int> result = intersect(nums1, nums2);
+    for(auto num : result)
+        std::cout << num << '\t';
+    std::cout << std::endl;
     return 0;
 }
